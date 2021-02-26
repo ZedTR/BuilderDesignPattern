36e2b4bf0ae7efcbd8810ae331ccb5467e303052
DESKTOP-DBQN56P\Zed <johnny.t.11@hotmail.com>
2021-02-25 19:35:02 -0600

    display built house

--- BuilderDesignPattern/Director.cs
+++ BuilderDesignPattern/Director.cs
@@ -10,14 +10,19 @@ namespace BuilderDesignPattern
 
         public IBuilder Builder {  set => _builder = value; }
 
+        private  House house = new House();
+
         public void BuildHouse()
         {
+            
             _builder.BuildTiles();
             _builder.BuildWalls();
             _builder.BuildWindows();
             _builder.SetElectrics();
             _builder.SetInteriors();
-
+            house = _builder.ShowHouse();
+            Console.WriteLine(house.DisplayComponents());
+            
         }
 
 
